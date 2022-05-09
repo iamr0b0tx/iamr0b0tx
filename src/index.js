a2e31a45965ccab9b6beedf909e53a79e39ecbcf
motuncoded <motunrayoadeneye@example.com>
2022-05-09 14:11:46 +0100

    final-commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
-import { StrictMode } from 'react/cjs/react.production.min';
 
 ReactDOM.render(
 	<BrowserRouter forceRefresh={true}>
